d27fbfb90e85c1aaa7dd909196054dfd34725082
miemo <keiko.takeda.78@gmail.com>
2021-07-03 07:36:18 +0900

    cookieにドメインを追加

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express =  require('express')
 const app1 = express()
 const cookie1 = (req,res,next) =>{
     res.cookie("first-cookie","hoge",{
-
+        domain:'.hoge.com',
         httpOnly: true,
     });
     next();
@@ -19,6 +19,7 @@ const app2 = express()
 
 const cookie2 = (req,res,next) =>{
     res.cookie("third-cookie","fuga",{
+        domain:'6776a3fa2177.ngrok.io',
         httpOnly: true,
     });
     next();
@@ -26,4 +27,4 @@ const cookie2 = (req,res,next) =>{
 app2.use(cookie2)
 app2.use(express.static('statics'))
 //ngrokは立ち上げる度にURLが変わるので都度変更すること
-app2.listen(80,() => console.log('Listening on port 80...https://050a086837ab.ngrok.io/test1'))
+app2.listen(80,() => console.log('Listening on port 80...https://6776a3fa2177.ngrok.io/test1.html'))
